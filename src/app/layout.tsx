e514169870054f215c913577e87e1c092143b87e
AnkitShome <ankitshome7@gmail.com>
2025-06-10 17:34:52 +0530

    Shad cn and strean apis setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import { ClerkProvider } from '@clerk/nextjs';
-import '../styles/globals.css';
+import './globals.css';
 
 export const metadata = { title: 'Streamy' };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
    return (
-      <html lang="en">
+      <html lang="en" className='dark'>
          <body>
             <ClerkProvider>
                {children}
